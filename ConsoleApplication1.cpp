668ba59df3676dda41fa1e2fcd9158d439384591
Miliforo <2052851-Miliforo@users.noreply.gitlab.com>
2023-01-11 20:33:53 +0300

    Cmake test

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+
+void version();
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Version: " << version() << std::endl;
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
